9a67128eca9646d165215f6bc6f1f57123425c30
boss1323 <70810620+boss1323@users.noreply.github.com>
2025-07-20 16:30:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '649616116972257290',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
